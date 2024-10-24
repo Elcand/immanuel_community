1c397d55920c5b554425fb072d2f7d6389873be6
costaganteng <ardianus.caesar3@smk.belajar.id>
2024-10-24 16:04:15 +0700

    admin tampilan

---
+++ admin/layouts/content.php
@@ -0,0 +1,19 @@
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <p>Welcome to Immanuel Community!</p>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ admin/layouts/sidebar.php
@@ -0,0 +1,29 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <a  class="brand-link">
+    <span class="brand-text font-weight-light">Admin Immanuel Community</span>
+  </a>
+  <div class="sidebar">
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+          <a href="dashboard.php" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="users.php" class="nav-link">
+            <i class="nav-icon fas fa-users"></i>
+            <p>Users</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="settings.php" class="nav-link">
+            <i class="nav-icon fas fa-cog"></i>
+            <p>Settings</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</aside>
