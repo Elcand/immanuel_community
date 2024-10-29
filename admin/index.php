34cf5ecddeb454af564e2a6cf16c18759249cd21
costaganteng <ardianus.caesar3@smk.belajar.id>
2024-10-29 15:33:06 +0700

    tampilan baru admin

--- admin/index.php
+++ admin/index.php
@@ -1,32 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<? include('includes/header.php'); ?>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin Immanuel Community</title>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <link rel="stylesheet" href="assets-admin/plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" href="assets-admin/dist/css/adminlte.min.css">
-</head>
+<h4>Hi Admin Immanuel Community</h4>
 
-<body class="hold-transition sidebar-mini layout-fixed">
-  <div class="wrapper">
-
-    <?php include('layouts/navbar.php'); ?>
-    <?php include('layouts/sidebar.php'); ?>
-
-    <div class="content-wrapper">
-      <?php include('layouts/content.php'); ?>
-    </div>
-
-    <?php include('layouts/footer.php'); ?>
-  </div>
-  <!-- ./wrapper -->
-
-  <script src="assets-admin/plugins/jquery/jquery.min.js"></script>
-  <script src="assets-admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets-admin/dist/js/adminlte.min.js"></script>
-</body>
-
-</html>
+<? include('includes/footer.php'); ?>
